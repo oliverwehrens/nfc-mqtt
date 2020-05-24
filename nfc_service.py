bbfa66f605f914a433b7f540f965a65f4f1e0272
Oliver Wehrens <oliver.wehrens@polyteia.de>
2020-05-24 18:15:13 +0200

    Outputting some more infos.

--- nfc_service.py
+++ nfc_service.py
@@ -1,3 +1,5 @@
+import time
+
 import nfc
 import paho.mqtt.client as mqtt
 
@@ -7,10 +9,12 @@ import paho.mqtt.client as mqtt
 class TagScan:
     def __init__(self, client_mqtt):
         self.last_known_tag_id = None
+        self.tag_connect_time = 0
         self.client = client_mqtt
         self.terminate = False
 
     def run(self):
+        print("Starting ..")
         clf = nfc.ContactlessFrontend("usb")
         rdwr_options = {
             "on-connect": self.on_tag_connect,
@@ -18,17 +22,23 @@ class TagScan:
             "beep-on-connect": False,
         }
         while not self.terminate:
+            print("Connecting.")
             clf.connect(rdwr=rdwr_options, terminate=lambda: self.terminate)
+        clf.close()
 
     def on_tag_connect(self, tag):
+        print("Tag connect: " + str(tag))
+        now = time.time()
         tag_id = str(tag).split(" ")[-1][3:-1]
-        if self.last_known_tag_id != tag_id:
+        if self.last_known_tag_id != tag_id or now > self.tag_connect_time + 1:
             json = '{"uid":"' + tag_id + '", "response":"' + str(tag) + '"}'
             self.client.publish("nfc/tag", json)
             self.last_known_tag_id = tag_id
+            self.tag_connect_time = time.time()
         return True
 
     def on_tag_release(self, tag):
+        print("Tag release: " + str(tag))
         return True
 
 
